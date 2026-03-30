afd1efbd2bd9122252ef1381ef1c08bedd4b8216
crarthur2 <pradoarthur2004@gmail.com>
2026-03-30 19:47:35 -0300

    configurando conexao postgres sqlalchemy

---
+++ test_sql.py
@@ -0,0 +1,46 @@
+import os
+import urllib.parse
+from dotenv import load_dotenv
+from sqlalchemy import  create_engine, Column, Integer, String, Float, DateTime
+from sqlalchemy.orm import declarative_base, sessionmaker
+from datetime import datetime
+
+os.environ['PGMESSAGES'] = 'en_US.UTF-8'
+load_dotenv()
+
+user = os.getenv('DB_USER', 'postgres')
+password = urllib.parse.quote_plus(os.getenv('DB_PASSWORD',''))
+host = os.getenv('DB_HOST', '172.23.112.1')
+port = os.getenv('DB_PORT','5432')
+db = os.getenv('DB_NAME', 'mouse_tracker')
+
+DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{db}"
+engine = create_engine(DATABASE_URL)
+
+Base = declarative_base()
+
+class MousePreco(Base):
+    __tablename__ = 'preco_mouses'
+
+    id = Column(Integer, primary_key=True)
+    nome_produto = Column(String, nullable=False)
+    loja = Column(String, nullable=False)
+    preco = Column(Float, nullable=False)
+    data_coleta = Column(DateTime, default=datetime.now)
+
+
+if __name__ == '__main__':
+    try:
+        Base.metadata.create_all(engine)
+        print(f"Conexão estável e tabela 'precos_mouses pronta no banco '{db}'!")
+    except Exception as e:
+        print(f"Erro ao configurar Banco de Dados: {e}")
+
+
+
+
+
+
+
+
+
---
+++ teste_scrapping.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.kabum.com.br/produto/495544/mouse-gamer-sem-fio-logitech-g-pro-x-superlight-2-com-lightspeed-32000-dpi-sensor-hero-2-com-bateria-recarregavel-preto-910-006629"
+
+headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:149.0) Gecko/20100101 Firefox/149.0'}
+
+response = requests.get(url, headers=headers)
+
+print(f"Status da resposta: {response.status_code}")
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    print(f"Titulo da Página: {soup.title.text}")
+
+preco_elemento = soup.find("h4", class_="text-4xl")
+
+if preco_elemento:
+    print(f"Produto encontrado, preço: {preco_elemento.get_text()}")
+else:
+    print("Produto nao encontrado")
+
+preco_bruto = preco_elemento.get_text()
+
+novo_preco = preco_bruto.replace("R$","").strip()
+
+novo_preco = novo_preco.replace(",",".")
+
+preco_final = float(novo_preco)
+
+print(f"Preco formatado: {preco_final}")
+print(f"Tipo: {type(preco_final)}")
